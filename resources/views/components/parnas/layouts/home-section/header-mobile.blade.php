ecfd6d8297f9cd737db1cea4f9b69b8682dc4e36
milad <miladrealkord@gmail.com>
2022-07-11 13:45:04 +0430

    mobile

--- resources/views/components/parnas/layouts/home-section/header-mobile.blade.php
+++ resources/views/components/parnas/layouts/home-section/header-mobile.blade.php
@@ -1,3 +1,59 @@
-<section class="mobile-header">
+<section style="display: none" x-data x-show="window.innerWidth<868" class="header-mobile">
+    <div class="pattern"></div>
+    <div class="top-header-mobile w-100 d-flex flex-column align-items-center">
+        <div class="w-90 mx-auto flex-between p-header-desktop">
+            <div class="name-parnas">
+                <h1>آژانس خلاقیت پارناس</h1>
+                <h2>ایــــدتـو ، ایـــــده آل کــــن !</h2>
+            </div>
+            <a href="/" class="logo-website">
+                <svg style="width: 60px !important;height: 62px !important;" viewBox="0 0 106.636 90.916">
+                    <g id="Logo_Parnas" data-name="Logo Parnas" transform="translate(2.194 2.463)">
+                        <g id="Layer_1" data-name="Layer 1" transform="translate(0 0)">
+                            <path id="Path_1344" data-name="Path 1344" d="M88.751,78.021,56.645,21.66,15.31,93.516"
+                                  transform="translate(-5.213 -6.77)" fill="none" stroke="#ca7e65"
+                                  stroke-linecap="round" stroke-miterlimit="10" stroke-width="2.5"/>
+                            <path id="Path_1345" data-name="Path 1345"
+                                  d="M55.432,72.121H73.938L62.121,50.98,42.19,86.04"
+                                  transform="translate(-10.689 -12.743)" fill="none" stroke="#fff"
+                                  stroke-linecap="round" stroke-miterlimit="10" stroke-width="2.5"/>
+                            <path id="Path_1351" data-name="Path 1351"
+                                  d="M55.5,74.709l59.087-.5L72.341,2.96l-5.232,8.934,29.51,50.444H89.1"
+                                  transform="translate(-12.325 -2.96)" fill="none" stroke="#ca7e65"
+                                  stroke-linecap="round" stroke-miterlimit="10" stroke-width="2.5"/>
+                            <path id="Path_1352" data-name="Path 1352"
+                                  d="M14.5,74.211H2.63L44.865,2.96,50.1,11.894,20.586,62.339"
+                                  transform="translate(-2.63 -2.96)" fill="none" stroke="#ca7e65" stroke-linecap="round"
+                                  stroke-miterlimit="10" stroke-width="2.5"/>
+                        </g>
+                    </g>
+                </svg>
 
+            </a>
+            <div class="l-header">
+                <div class="contact-header">
+                    <a class="header-box-menu" href="">
+                        <svg id="_032-pattern-1" data-name="032-pattern-1" width="19" height="20"
+                             viewBox="0 0 27.742 27.742">
+                            <g id="Group_112" data-name="Group 112" transform="translate(0 15.67)">
+                                <g id="Group_111" data-name="Group 111">
+                                    <path id="Path_1480" data-name="Path 1480"
+                                          d="M21.706,294.423a.813.813,0,0,0,0,1.626h5.223a.813.813,0,0,0,.813-.813v-5.223a.813.813,0,0,0-.813-.813H16.483a.813.813,0,0,0-.813.813v9.634H1.626v-8.821h8.821v3.6H6.036a.813.813,0,0,0,0,1.626h5.223a.813.813,0,0,0,.813-.813v-5.223a.813.813,0,0,0-.813-.813H.813a.813.813,0,0,0-.813.813v10.447a.813.813,0,0,0,.813.813H26.929a.813.813,0,1,0,0-1.625H17.3v-8.821h8.821v3.6Z"
+                                          transform="translate(0 -289.2)" fill="#ca7e65"/>
+                                </g>
+                            </g>
+                            <g id="Group_114" data-name="Group 114" transform="translate(0 0)">
+                                <g id="Group_113" data-name="Group 113">
+                                    <path id="Path_1481" data-name="Path 1481"
+                                          d="M26.929,0H16.483a.813.813,0,0,0-.813.813V6.036a.813.813,0,0,0,.813.813h5.223a.813.813,0,0,0,0-1.626H17.3v-3.6h8.821v8.821H12.072V.813A.813.813,0,0,0,11.259,0H.813A.813.813,0,0,0,0,.813V6.036a.813.813,0,0,0,.813.813H6.036a.813.813,0,0,0,0-1.626H1.626v-3.6h8.821v8.821H.813a.813.813,0,0,0,0,1.626H26.929a.813.813,0,0,0,.813-.813V.813A.813.813,0,0,0,26.929,0Z"
+                                          transform="translate(0 0)" fill="#ca7e65"/>
+                                </g>
+                            </g>
+                        </svg>
+
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
